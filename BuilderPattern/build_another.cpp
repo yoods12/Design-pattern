b21523bdccbdbfbe0ca7236905c928b12c446caf
BoHyeonSong <143975366+BoHyeonSong@users.noreply.github.com>
2023-11-19 22:04:55 +0900

    Update build_another.cpp
    변수 이름 변경
    int_var, string_var, float_var함수 반환자료형 &참조형으로 변환

--- BuilderPattern/build_another.cpp
+++ BuilderPattern/build_another.cpp
@@ -1,45 +1,45 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 class test { // 만들어질 클래스의 형태
     // 각각 int, string, float 자료형의 변수를 하나씩 가지고 있음
-    int var1;
-    string var2;
-    float var3;
+    int varI;
+    string varS;
+    float varF;
 public:
     // builder 클래스에서 build함수로 반환될때 test 클래스를 생성해서 반환하기때문에
     // 생성자를 미리 지정해줘야함
-    test(int v1, string v2, float v3) { 
-        var1 = v1;
-        var2 = v2;
-        var3 = v3;
+    test(int vI, string vS, float vF) { 
+        varI = vI;
+        varS = vS;
+        varF = vF;
     }
     void show() {// 해당 클래스가 가지는 원소들
-        cout << "int_var : " << var1 << endl;
-        cout << "string_var : " << var2 << endl;
-        cout << "float_var : " << var3 << endl;
+        cout << "int_var : " << varI << endl;
+        cout << "string_var : " << varS << endl;
+        cout << "float_var : " << varF << endl;
     }
     class builder { // test클래스 객체를 생성하기 위한 builder클래스
         //생성할 클래스와 같은 자료형의 변수들을 가지고 있어야한다
         //그래야 해당 값을 그대로 넘겨줄 수 있다.
-        int var1;
-        string var2;
-        float var3;
+        int varI;
+        string varS;
+        float varF;
     public:
-        builder int_var(int v) { // int형 자료에 값을 넣는 set함수이다.
-            var1 = v;
+        builder& int_var(int v) { // int형 자료에 값을 넣는 set함수이다.
+            varI = v;
             return *this; // 여기서 자기자신을 호출해서 해당 클래스의 다른 함수로 이어갈 수 있다.
         }
-        builder string_var(string v) {// string형 자료에 값을 넣는 set함수이다.
-            var2 = v;
+        builder& string_var(string v) {// string형 자료에 값을 넣는 set함수이다.
+            varS = v;
             return *this;
         }
-        builder float_var(float v) {// float형 자료에 값을 넣는 set함수이다.
-            var3 = v;
+        builder& float_var(float v) {// float형 자료에 값을 넣는 set함수이다.
+            varF = v;
             return *this;
         }
         test build() { // 위 함수들로 기입한 데이터를 통해 test클래스를 만들고 그를 반환한다
-            test t(var1, var2, var3);
+            test t(varI, varS, varF);
             return t;
         }
     };
@@ -48,7 +48,8 @@ public:
 int main()
 {
     test::builder build; // 미리 builder 클래스의 build를 만든다 클래스 변수의 선언과 동시에 함수의 호출은 불가능하기 때문
-    test t = build.int_var(10) // 원래는 test t = build.int_var(10).string_var("abc") .float_var(10.5).build(); 이런 식이나 가독성을 위해 들여쓰기를 했다
+    test t = build// 원래는 test t = build.int_var(10).string_var("abc") .float_var(10.5).build(); 이런 식이나 가독성을 위해 들여쓰기를 했다
+        .int_var(10) 
         .string_var("abc") 
         .float_var(10.5)
         .build();
